b3299614dc940a88e7f0f4491de06ec68a020eaf
Tatiana <ttt-ttt@bk.ru>
2022-07-03 01:40:34 +0300

    Task9_Method

---
+++ Task9/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа
+int n = new Random().Next(10, 100);
+Task9(n);
+
+void Task9(int currentnumber)
+{
+Console.WriteLine("Число:" + currentnumber);
+int des = currentnumber / 10;
+int ed = currentnumber % 10;
+if (ed > des)
+    Console.WriteLine(ed);
+else if (ed < des)
+    Console.WriteLine(des);
+else
+    Console.WriteLine("Цифры числа равны.");
+}
